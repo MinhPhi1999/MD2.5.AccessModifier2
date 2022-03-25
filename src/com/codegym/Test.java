b992013614b0a511c2e2f0c971a21cba76087de7
unknown <qquangmminh19999@gmail.com>
2022-03-25 14:02:16 +0700

    bt2

---
+++ src/com/codegym/Test.java
@@ -0,0 +1,12 @@
+package com.codegym;
+
+public class Test {
+
+    public static void main(String[] args) {
+	// write your code here
+        Student student = new Student();
+        student.setName("Minh");
+        student.setClasses("I1");
+        System.out.println(student);
+    }
+}
